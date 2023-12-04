d2424c002a07dd7b5ba8691e98b1b67be5760908
Kedar More <kedar.more@calsoftinc.com>
2023-12-04 22:14:41 +0530

    first commit

---
+++ asset/main.js
@@ -0,0 +1,38 @@
+  function bigImg(x) {
+                x.style.height = "55px";
+                x.style.width = "125px";
+                }
+  function normalImg(x) {
+                x.style.height = "60px";
+                x.style.width = "130px";
+                }
+				
+  
+document.getElementById("myAnchor").addEventListener("click", function() {
+ console.log("Anchor clicked");
+  this.style.color = "yellow"; this.text-decoration: "underline";
+});
+
+  //function css_mani(){
+		//		       //alert("hello");
+		//			   var x;
+					//   x=document.getElementById("myAnchor");
+					  //alert(x.style.color+" "+x.style.backgroundColor);
+				 //  x.style.color="#00cc00";
+					  // x.style.backgroundColor="red";
+			//	}
+			  
+			  
+			  y=document.getElementById("aboutid");
+					  //alert(x.style.color+" "+x.style.backgroundColor);
+			   y.style.color="#00cc00";
+				   
+				   
+			  z=document.getElementById("serviceid");
+					  //alert(x.style.color+" "+x.style.backgroundColor);
+		      z.style.color="#00cc00";
+				   
+				   
+			  a=document.getElementById("contactid");
+					  //alert(x.style.color+" "+x.style.backgroundColor);
+		  	   a.style.color="#00cc00";
